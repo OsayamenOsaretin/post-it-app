77a4cdaa1542f660e7603df4bed46073eba97cbc
yamen <osaretinosayamen@gmail.com>
2017-05-24 04:59:45 +0100

    Add sign up feature files, server.js, server/routes/index.js, server/routes/sign_up_route.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 // Set up express
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 
 // import firebase
 const firebase = require('firebase');
@@ -12,14 +12,14 @@ const port = process.env.PORT || 6969;
 
 // configure firebase
 const config = {
-    apiKey: "AIzaSyAOCl6QRw5NYGGENE8URKteNO1rV7f1yo8",
-    authDomain: "post-it-69a9a.firebaseapp.com",
-    databaseURL: "https://post-it-69a9a.firebaseio.com",
-    projectId: "post-it-69a9a",
-    storageBucket: "post-it-69a9a.appspot.com",
-    messagingSenderId: "383450311400"
-  };
-  firebase.initializeApp(config);
+  apiKey: 'AIzaSyAOCl6QRw5NYGGENE8URKteNO1rV7f1yo8',
+  authDomain: 'post-it-69a9a.firebaseapp.com',
+  databaseURL: 'https://post-it-69a9a.firebaseio.com',
+  projectId: 'post-it-69a9a',
+  storageBucket: 'post-it-69a9a.appspot.com',
+  messagingSenderId: '383450311400',
+};
+firebase.initializeApp(config);
 
 
 const db = firebase.database();
@@ -32,11 +32,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POSTS');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, \
-		content-type, Authorization');
-	next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POSTS');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  next();
 });
 
+require('./routes')(app, db);
 
+app.listen(port, () => {
+  // console.log(`We are live on ${port}`);
+});
