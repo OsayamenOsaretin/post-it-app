0060b5da5ccafc4d31dce95317083366b809d655
yamen <osaretinosayamen@gmail.com>
2017-05-24 14:12:20 +0100

    Edit signup route to use firebase parameter instead of db

--- server/routes/user_signup_route.js
+++ server/routes/user_signup_route.js
@@ -1,21 +1,21 @@
 // routes to user sign-up
 
 // require firebase to create user
-const firebase = require('firebase');
 
-const auth = firebase.auth();
 
-module.exports = function (app) {
+module.exports = (app, firebase) => {
   app.post('/user/signup', (req, res) => {
     // perfom user signup here
     const userName = req.body.userName;
     const email = req.body.email;
     const password = req.body.password;
-    auth.createUserWithEmailAndPassword(email, password).then((user) => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((user) => {
       user.updateProfile({
         displayName: userName,
       }).then(() => {
         // console.log('Username updated successfully');
+        console.log(user);
       });
       res.send({ message: 'Sign up Successful!' });
     }).catch(() => {
