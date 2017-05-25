235573d897b73c00b813e9ef6b32c647afc275dd
yamen <osaretinosayamen@gmail.com>
2017-05-25 14:24:34 +0100

    Modify routes for creating groups

--- server/routes/create_group_routes.js
+++ server/routes/create_group_routes.js
@@ -6,6 +6,8 @@ module.exports = (app, firebase) => {
   app.post('/group', (req, res) => {
     const groupName = req.body.groupName;
     // create group with this unfathomable block of code
+
+    // check that a user is signed in before you try to add group
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         // This means a user is signed in
@@ -15,19 +17,23 @@ module.exports = (app, firebase) => {
         const newGroupKey = db.ref().child('groups').push({
           groupname: groupName,
           creator: userId,
-          users: [
-            userId,
-          ],
         }).key;
 
-        db.ref('/users/' + userId).child('groups').push({
-          groupKey: newGroupKey,
-          name: groupName,
+        db.ref().child(`groups/${newGroupKey}/users/${userId}`).set({
+          Id: userId,
         });
+
+        db.ref(`/users/${userId}/groups/`).child(newGroupKey).set(
+          { name: groupName }
+          );
 
 
         res.send({
           message: 'Created Group',
+        });
+      } else {
+        res.send({
+          message: 'You are not signed in right now!'
         });
       }
     });
