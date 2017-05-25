4d354d94c6ca430a5bcbd254c284a97e665159c8
yamen <osaretinosayamen@gmail.com>
2017-05-25 14:24:54 +0100

    Add the route file for adding users to groups

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 const signUpRoute = require('./user_signup_route');
 const signInRoute = require('./user_signin_route');
 const createGroup = require('./create_group_routes');
+const addUser = require('./add_user_group_routes.js');
 
 module.exports = (app, firebase) => {
   // this signs up the user
   signUpRoute(app, firebase);
   signInRoute(app, firebase);
   createGroup(app, firebase);
+  addUser(app, firebase);
 };
