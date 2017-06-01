bc52b65a7a20874b19284c68247cc043637dcc62
Yamen <osaretinosayamen@outlook.com>
2017-06-01 08:35:43 +0100

    undo harmful changes to server.js

--- server/server.js
+++ server/server.js
@@ -12,16 +12,16 @@ const app = express();
 // configure port
 const port = process.env.PORT || 6969;
 
-// // configure firebase
-// const config = {
-//   apiKey: 'AIzaSyAOCl6QRw5NYGGENE8URKteNO1rV7f1yo8',
-//   authDomain: 'post-it-69a9a.firebaseapp.com',
-//   databaseURL: 'https://post-it-69a9a.firebaseio.com',
-//   projectId: 'post-it-69a9a',
-//   storageBucket: 'post-it-69a9a.appspot.com',
-//   messagingSenderId: '383450311400',
-// };
-// firebase.initializeApp(config);
+// configure firebase
+ const config = {
+   apiKey: 'AIzaSyAOCl6QRw5NYGGENE8URKteNO1rV7f1yo8',
+   authDomain: 'post-it-69a9a.firebaseapp.com',
+   databaseURL: 'https://post-it-69a9a.firebaseio.com',
+   projectId: 'post-it-69a9a',
+   storageBucket: 'post-it-69a9a.appspot.com',
+   messagingSenderId: '383450311400',
+ };
+ firebase.initializeApp(config);
 
 // body parser, used to grab information from POST requests
 app.use(bodyParser.urlencoded({ extended: true }));
