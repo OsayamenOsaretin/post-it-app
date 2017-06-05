a2ce1405c5788eb0fc603366d653164b84534288
yamen <osaretinosayamen@gmail.com>
2017-06-05 14:28:08 +0100

    add correct configuration files

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,32 @@
-import path from 'path';
-import webpack from 'webpack';
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 const config = {
-  devtool: 'eval-source-map',
+  devtool: 'source-map',
 
   entry: [
     'webpack-hot-middleware/client',
-    path.join(__dirname, './main.js')],
+    path.join(__dirname, './client/src/main.js')],
 
   output: {
-    path: path.join(__dirname, '/dist/'),
-    publicPath: '/',
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    
   },
 
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './client/src/index.html'
+    })
   ],
 
   module: {
     loaders: [
       {
-        test: /\.js?$/,
+        test: /\.(js)$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
 
