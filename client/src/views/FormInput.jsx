cc0fc0806d839b5d60fdc0b4c7052b6b62a707da
yamen <osaretinosayamen@gmail.com>
2017-06-07 12:28:36 +0100

    refactor Form Input name

---
+++ client/src/views/FormInput.jsx
@@ -0,0 +1,61 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ *
+ */
+export default class FormInput extends Component {
+
+ /**
+  *
+  * @param {*} props
+  */
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fieldInput: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+/**
+ * @returns {void}
+ * @param {*} event
+ */
+  handleChange(event) {
+    const value = event.target.value;
+
+    this.setState(() => ({
+      fieldInput: value,
+    })
+    );
+
+    this.props.handleChange(this.state.fieldInput, this.props.label);
+  }
+
+/**
+ * renders the Form input field component
+ * @returns {void}
+ * @memberof FormInput
+ */
+  render() {
+    return (
+      <div className = "inputField">
+      <input
+      id = 'formInput'
+      placeholder = {this.props.label}
+      type ='text'
+      autoComplete = 'off'
+      value = {this.state.fieldInput}
+      onChange = {this.handleChange} />
+
+      </div>
+    );
+  }
+}
+
+FormInput.propTypes = {
+  label: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
