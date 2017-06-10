5db3520a4794a450f8ec371a803c998cc6754a70
yamen <osaretinosayamen@gmail.com>
2017-06-10 18:08:30 +0100

    refactor landing page container and main to jsx files

---
+++ client/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LandingPageContainer from './views/LandingPageContainer';
+import './index.css';
+
+ReactDOM.render(<LandingPageContainer />, document.getElementById('app'));
+
+
