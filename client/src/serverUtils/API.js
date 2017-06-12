27d1e027588302213ad51c8607b8bf443a14451a
yamen <osaretinosayamen@gmail.com>
2017-06-12 16:24:00 +0100

    add api module that abstracts away api calls

---
+++ client/src/serverUtils/API.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import PostItActionTypes from '../data/PostItActionTypes';
+import PostItDispatcher from '../data/PostItDispatcher';
+import { recieveGroups } from '../data/postItActions/postItServerActions';
+
+/**
+ * getGroups makes an api call for user's groups and dispatches(res) to registered listeners
+ * @return {void}
+ */
+export function getGroups() {
+  axios.get('/groups')
+  .then((res) => {
+    // call server action to handle recieved groups info
+    recieveGroups(res);
+  })
+  .catch((err) => {
+    PostItDispatcher.handleViewAction({
+      actionType: PostItActionTypes.FAILED_GROUPS,
+      error: err.message
+    });
+  });
+}
+/**
+ * addGroupApi makes an api call to add a group to user's group.
+ * @param {*} groupName
+ * @return {void}
+ */
+export function addGroupApi(groupName) {
+  axios.post('/group', {
+    body: groupName,
+  })
+  .then((res) => {
+    if (res.status === 200) {
+      // make api call to get all the new groups
+      getGroups();
+    }
+  });
+}
