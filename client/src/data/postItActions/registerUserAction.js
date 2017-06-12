8ac90d0c2324cae9f8f1a4ed57a227058c144a0f
yamen <osaretinosayamen@gmail.com>
2017-06-12 16:16:49 +0100

    refactor register user action to use postit dispatcher, and check for accurate response status

--- client/src/data/postItActions/registerUserAction.js
+++ client/src/data/postItActions/registerUserAction.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import PostItActionTypes from '../PostItActionTypes';
+import PostItDispatcher from '../PostItDispatcher';
 
 
 /**
@@ -10,22 +11,29 @@ import PostItActionTypes from '../PostItActionTypes';
  * @param {newUserDetails} newUserDetails
  */
 export default newUserDetails => (
-  (dispatch) => {
     axios.post('user/signup', {
       body: newUserDetails
     })
-    .then(() => {
-      dispatch({
-        type: PostItActionTypes.LOGIN_USER,
-      });
+    .then((res) => {
+      if (res.status === 200) {
+        // dispatch action to Login user on successful registration
+        PostItDispatcher.handleViewAction({
+          actionType: PostItActionTypes.LOGIN_USER,
+        });
+      } else {
+        PostItDispatcher.handleViewAction({
+          actionType: PostItActionTypes.FAILED_REGISTER,
+          error: res.message,
+          status: 'failed'
+        });
+      }
     })
     .catch((err) => {
-      dispatch({
-        type: PostItActionTypes.FAILED_REGISTER,
-        err: err.message,
+      PostItDispatcher.handleViewAction({
+        actionType: PostItActionTypes.FAILED_REGISTER,
+        error: err.message,
         status: 'failed'
       });
-    });
-  }
+    })
 );
 
