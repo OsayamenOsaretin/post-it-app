24c0e3987c0c65e0c9bff659345e25112b62af7d
yamen <osaretinosayamen@gmail.com>
2017-06-12 16:19:15 +0100

    refactor user signin action to use postit dispatcher, check for accurate response status code

--- client/src/data/postItActions/userSignInAction.js
+++ client/src/data/postItActions/userSignInAction.js
@@ -11,21 +11,32 @@ import PostItActionTypes from '../PostItActionTypes';
  */
 
 export default userDetails => (
-  () => {
     axios.post('user/signin', {
       body: userDetails
     })
-    .then(() => {
-      PostItDispatcher.handleAction({
-        type: PostItActionTypes.GET_GROUPS,
-      });
+    .then((res) => {
+      if (res.status === 200) {
+        // dispatch action to get groups(view) in the case of successful login
+        PostItDispatcher.handleViewAction({
+          actionType: PostItActionTypes.GET_GROUPS,
+        });
+        // dispatch action to get groups(server) in the case of successful login
+        PostItDispatcher.handleServerAction({
+          actionType: PostItActionTypes.GET_GROUPS
+        });
+      } else {
+        PostItDispatcher.handleViewAction({
+          actionType: PostItActionTypes.FAILED_SIGNIN,
+          error: res.message,
+          status: 'failed'
+        });
+      }
     })
     .catch((err) => {
-      PostItDispatcher.handleAction({
-        type: PostItActionTypes.FAILED_SIGNIN,
-        err: err.message,
+      PostItDispatcher.handleViewAction({
+        actionType: PostItActionTypes.FAILED_SIGNIN,
+        error: err.message,
         status: 'failed'
       });
-    });
-  }
+    })
 );
