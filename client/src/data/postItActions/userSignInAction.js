ac1c86fe1d0f1b8ceb93c937554cfb31141e4635
yamen <osaretinosayamen@gmail.com>
2017-06-16 11:43:19 +0100

    change from axios to superagent

--- client/src/data/postItActions/userSignInAction.js
+++ client/src/data/postItActions/userSignInAction.js
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import request from 'superagent';
 import PostItDispatcher from '../PostItDispatcher';
 import PostItActionTypes from '../PostItActionTypes';
+
 
 /**
  *userSignInAction - signs in user & dispatches actions to get groups
@@ -11,34 +12,14 @@ import PostItActionTypes from '../PostItActionTypes';
  */
 
 export default (userDetails) => {
-    axios.post('user/signin', {
-      email: userDetails.email,
-      password: userDetails.password
+  console.log('superagent api call to login');
+  request
+  .post('user/signin')
+  .send(userDetails)
+  .end((err, res) => {
+    console.log('api call returned a result');
+    if (err) {
+      console.log(err);
     }
-    )
-    .then((res) => {
-      if (res.status === 200) {
-        // dispatch action to get groups(view) in the case of successful login
-        PostItDispatcher.handleViewAction({
-          actionType: PostItActionTypes.GET_GROUPS,
-        });
-        // dispatch action to get groups(server) in the case of successful login
-        PostItDispatcher.handleServerAction({
-          actionType: PostItActionTypes.GET_GROUPS
-        });
-      } else {
-        PostItDispatcher.handleViewAction({
-          actionType: PostItActionTypes.FAILED_SIGNIN,
-          error: res.message,
-          status: 'failed'
-        });
-      }
-    })
-    .catch((err) => {
-      PostItDispatcher.handleViewAction({
-        actionType: PostItActionTypes.FAILED_SIGNIN,
-        error: err.message,
-        status: 'failed'
-      });
-    });
+  });
 };
