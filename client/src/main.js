d766548465fb7e00bdc09196ac57db81737cdb88
yamen <osaretinosayamen@gmail.com>
2017-06-17 06:38:00 +0100

    render the app container component

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LandingPageContainer from './views/LandingPageContainer.jsx';
+import LandingPageContainer from './views/LoginRegisterContainer/LandingPageContainer.jsx';
+import App from './views/App.jsx';
 import './index.css';
 
-ReactDOM.render(<LandingPageContainer />, document.getElementById('app'));
-
+ReactDOM.render(<App />, document.getElementById('app'));
 
