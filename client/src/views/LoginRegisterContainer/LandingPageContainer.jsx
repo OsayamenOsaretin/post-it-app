cd1ac023ee59f57fb8ecc609fc2f6ab245ee4cf6
yamen <osaretinosayamen@gmail.com>
2017-06-17 06:36:01 +0100

    refactor, move login and register views to seperate folder

---
+++ client/src/views/LoginRegisterContainer/LandingPageContainer.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import LoginRegisterComponent from './LoginRegisterContainer.jsx';
+
+import postItImage from '../../postIt.png';
+import mailboxImage from '../../mailbox.png';
+
+/**
+ * LandingPageContainer holds the landing page including register and login
+ * @returns {void}
+ */
+function LandingPageContainer() {
+  return (
+    <div className='landing-page-container'>
+      <div className='landing-img-container'>
+    <div className = "post-it-img">
+      <img src = {postItImage} />
+    </div>
+    <div className = "mail-box-img">
+      <img src = {mailboxImage} />
+    </div>
+    </div>
+    <div className = "login-register-container">
+      <LoginRegisterComponent />
+    </div>
+    </div>
+  );
+}
+
+module.exports = LandingPageContainer;
---
+++ client/src/views/LoginRegisterContainer/RegisterComponent.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import registerAction from '../../data/postItActions/registerUserAction';
+
+/**
+ *
+ */
+export default class RegisterForm extends React.Component {
+  /**
+  * Instantiates an instance of a React Component for Registration
+  *
+  */
+  constructor() {
+    super();
+
+    this.state = {
+      userName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+/**
+ * @memberof RegisterComponent
+ * @returns {void}
+ * @param {*} event
+ */
+  handleChange(event) {
+    const value = event.target.value;
+    this.setState({
+      [event.target.name]: value
+    });
+  }
+
+/**
+   * @memberof RegisterComponent
+   * @returns {void}
+   * @param {*} event
+   */
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.password === this.confirmPassword) {
+      registerAction({
+        userName: this.userName,
+        password: this.password,
+        email: this.email
+      });
+    }
+  }
+
+/**
+ * @memberof RegisterComponent
+ * @returns {void}
+ */
+  render() {
+    return (
+       <form className = 'inputform'>
+        <input
+        name = 'userName'
+        placeholder = 'username'
+        type = 'text'
+        autoComplete ='off'
+        value = {this.state.userName}
+        onChange = {this.handleChange}
+        />
+        <input
+        name = 'email'
+        placeholder = 'email'
+        type = 'text'
+        autoComplete ='off'
+        value = {this.state.email}
+        onChange = {this.handleChange}
+        />
+        <input
+        name = 'password'
+        placeholder = 'password'
+        type = 'text'
+        autoComplete ='off'
+        value = {this.state.password}
+        onChange = {this.handleChange}
+        />
+        <input
+        name = 'confirmPassword'
+        placeholder = 'confirmpassword'
+        type = 'text'
+        autoComplete ='off'
+        value = {this.state.confirmPassword}
+        onChange = {this.handleChange}
+        />
+        <button
+        className = 'button'
+        type = 'submit'
+        disabled = { !this.state.userName || !this.state.email ||
+         !this.state.password || !this.state.confirmPassword }
+         onClick = {this.handleSubmit}
+         >
+        Register
+        </button>
+      </form>
+    );
+  }
+}
+
