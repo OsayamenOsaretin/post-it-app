9ca74972736d0077b38c01fb6c8e017cbdf264bc
yamen <osaretinosayamen@gmail.com>
2017-06-20 08:38:03 +0100

    add group view

---
+++ client/src/views/GroupContainer/AddGroup.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import AddGroupIcon from '../../resources/add-group.png';
+import { addGroupApi } from '../../data/postItActions/groupActions';
+
+/**
+ * Add group adds a new group to list of user's groups
+ */
+class AddGroupView extends React.Component {
+
+  /**
+   * constructor creates new react component, initializes state
+   */
+  constructor() {
+    super();
+
+    this.state = {
+      fieldInput: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  /**
+   * handleChange sets input field state when user types
+   * @memberof AddGroup
+   * @return {void}
+   * @param {*} event
+   */
+  handleChange(event) {
+    const value = event.target.value;
+
+    this.setState(() => ({
+      fieldInput: value,
+    })
+    );
+  }
+
+  /**
+   * handleSubmit handles calling the sendmessage action when button is clicked
+   * @memberof AddGroup
+   * @return {void}
+   * @param {*} event
+   */
+  handleSubmit(event) {
+    event.preventDefault();
+    addGroupApi({ groupName: this.state.fieldInput });
+    this.setState({
+      fieldInput: ''
+    });
+  }
+
+  /**
+   * render renders the component
+   * @return {void}
+   * @memberof AddGroup
+   */
+  render() {
+    return (
+     <form className="addGroupForm">
+        <div className="add-group-inputField">
+        <input
+        id="newGroup"
+        placeholder="Group name"
+        type="text"
+        autoComplete="off"
+        value={this.state.fieldInput}
+        onChange={this.handleChange}
+        />
+        <button
+        className='add-group-button'
+        type='submit'
+        disabled={!this.state.fieldInput}
+        onClick={this.handleSubmit}>
+          <img src={AddGroupIcon} />
+        </button>
+        </div>
+     </form>
+    );
+  }
+}
+
+module.exports = AddGroupView;
