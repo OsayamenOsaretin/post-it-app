9e761632f2a7cb9efc08eee14651b46037655d04
yamen <osaretinosayamen@gmail.com>
2017-06-20 08:57:00 +0100

    add files for group body and messages

---
+++ client/src/views/GroupContainer/GroupBodyContainer/SendMessageView.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import SendIcon from '../../../resources/send.png';
+
+/**
+ * Renders view for sending message to a group
+ */
+class SendMessageView extends React.Component {
+
+  /**
+   * constructor creates new react component
+   * @param {*} props
+   */
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  /**
+   * handleChange sets input field state when user types
+   * @memberof SendMessageView
+   * @return {void}
+   * @param {*} event
+   */
+  handleChange(event) {
+    const value = event.target.value;
+
+    this.setState(() => ({
+      message: value,
+    })
+    );
+  }
+
+/**
+   * handleSubmit handles calling the sendmessage action when button is clicked
+   * @memberof SendMessageView
+   * @return {void}
+   * @param {*} event
+   */
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      message: ''
+    });
+  }
+
+  /**
+   * render renders the component
+   * @return {void}
+   * @memberof SendMessageView
+   */
+  render() {
+    return (
+     <form className="sendMessageForm">
+        <div className="send-message-inputField">
+        <input
+        id="newMessage"
+        type="text"
+        autoComplete="off"
+        value={this.state.message}
+        onChange={this.handleChange}
+        />
+        <button
+        className='add-group-button'
+        type='submit'
+        disabled={!this.state.fieldInput}
+        onClick={this.handleSubmit}>
+          <img src={SendIcon} />
+        </button>
+        </div>
+     </form>
+    );
+  }
+}
+
+module.exports = SendMessageView;
