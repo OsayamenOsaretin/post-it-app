0a7b602881970bc8b9fae692a78598efeaa3dec7
yamen <osaretinosayamen@gmail.com>
2017-06-21 14:11:29 +0100

    add action to get all the users from the firebase database

---
+++ client/src/data/postItActions/getAllUsersAction.js
@@ -0,0 +1,24 @@
+import request from 'superagent';
+import PostItActionTypes from '../PostItActionTypes';
+import PostItDispatcher from '../PostItDispatcher';
+
+/**
+ * getAllUsersAction - get all the PostIt registered users
+ * @returns {void}
+ */
+export default () => {
+  request
+  .get('/users')
+  .end((error, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const userList = result.body.userList;
+      console.log(userList);
+      PostItDispatcher.handleServerAction({
+        type: PostItActionTypes.RECIEVE_USERS,
+        users: userList
+      });
+    }
+  });
+};
