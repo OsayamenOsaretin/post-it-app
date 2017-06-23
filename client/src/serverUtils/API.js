01e92bea4fe96bdf2610190015a2dea1f8b4c2e6
yamen <osaretinosayamen@gmail.com>
2017-06-23 05:08:48 +0100

    remove unneeded utility file

--- client/src/serverUtils/API.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-import PostItActionTypes from '../data/PostItActionTypes';
-import PostItDispatcher from '../data/PostItDispatcher';
-import { recieveGroups } from '../data/postItActions/postItServerActions';
-
-
-
