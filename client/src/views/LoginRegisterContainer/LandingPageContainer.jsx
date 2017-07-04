ba0d528ac73cf5cc298fa45a51ea957706b75ca3
yamen <osaretinosayamen@gmail.com>
2017-07-04 14:45:48 +0100

    add test files changes

--- client/src/views/LoginRegisterContainer/LandingPageContainer.jsx
+++ client/src/views/LoginRegisterContainer/LandingPageContainer.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import LoginRegisterComponent from './LoginRegisterContainer.jsx';
 
-import postItImage from '../../resources/postIt.png';
-import mailboxImage from '../../resources/mailbox.png';
-
 /**
  * LandingPageContainer holds the landing page including register and login
  * @returns {void}
@@ -13,10 +10,8 @@ function LandingPageContainer() {
     <div className='landing-page-container'>
       <div className='landing-img-container'>
     <div className = "post-it-img">
-      <img src = {postItImage} />
     </div>
     <div className = "mail-box-img">
-      <img src = {mailboxImage} />
     </div>
     </div>
     <div className = "login-register-container">
