2e79e393c7566e6295a3fbf51368d957aa918060
yamen <osaretinosayamen@gmail.com>
2017-07-05 14:07:29 +0100

    add google sign in view

--- client/src/views/LoginRegisterContainer/LandingPageContainer.jsx
+++ client/src/views/LoginRegisterContainer/LandingPageContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import LoginRegisterComponent from './LoginRegisterContainer.jsx';
-
+import GoogleSignIn from './GoogleSignInComponent.jsx';
 /**
  * LandingPageContainer holds the landing page including register and login
  * @returns {void}
@@ -9,13 +9,14 @@ function LandingPageContainer() {
   return (
     <div className='landing-page-container'>
       <div className='landing-img-container'>
-    <div className = "post-it-img">
-    </div>
-    <div className = "mail-box-img">
-    </div>
-    </div>
+        <div className = "post-it-img">
+        </div>
+        <div className = "mail-box-img">
+        </div>
+      </div>
     <div className = "login-register-container">
       <LoginRegisterComponent />
+      <GoogleSignIn />
     </div>
     </div>
   );
