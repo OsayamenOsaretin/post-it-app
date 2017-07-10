1bf5d11e10d94e481b65b4aae0f0f2dc720c4dc5
yamen <osaretinosayamen@gmail.com>
2017-07-10 17:36:15 +0100

    add helper function to check if a message has been read

---
+++ client/src/utility/getReadStatus.js
@@ -0,0 +1,14 @@
+// get the read status for a message
+
+export default (message, displayName) => {
+  if (message.read) {
+    if (message.read[displayName]) {
+      return true;
+    }
+    Object.keys(message.read).forEach((key) => {
+      console.log(key);
+    });
+  } else {
+    return false;
+  }
+};
