12be40dd190f115e84c6e6f43d52751fff909d0c
yamen <osaretinosayamen@gmail.com>
2017-07-12 08:37:50 +0100

    refactor views

--- client/src/views/GroupContainer/Dashboard.jsx
+++ client/src/views/GroupContainer/Dashboard.jsx
@@ -6,7 +6,7 @@ import GroupStore from '../../data/postItStores/PostItGroupStore';
 import PostItDispacher from '../../data/PostItDispatcher';
 import PostItActionTypes from '../../data/PostItActionTypes';
 
-const socket = io('https://postit-app-develop.herokuapp.com/');
+const socket = io('http://localhost:6969');
 
 // https://postit-app-develop.herokuapp.com/
 
