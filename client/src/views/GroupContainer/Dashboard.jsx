63a9472b9d48efb63525b3595abe7833e727a02a
yamen <osaretinosayamen@gmail.com>
2017-07-18 11:33:41 +0100

    use app url for socket connection

--- client/src/views/GroupContainer/Dashboard.jsx
+++ client/src/views/GroupContainer/Dashboard.jsx
@@ -9,7 +9,7 @@ import getAllUsersAction from '../../data/postItActions/getAllUsersAction';
 import PostItDispatcher from '../../data/PostItDispatcher';
 import PostItActionTypes from '../../data/PostItActionTypes';
 
-const socket = io('http://localhost:6969');
+const socket = io('https://postit-app-develop.herokuapp.com/');
 
 // http://localhost:6969
 // https://postit-app-develop.herokuapp.com/
