a735da0435ca0c774be83aba8bb8e78a5b7ae778
yamen <osaretinosayamen@gmail.com>
2017-08-04 14:18:10 +0100

    move logic within authentication callback to solve duplicate message problem

--- server/routes/send_message_route.js
+++ server/routes/send_message_route.js
@@ -4,13 +4,13 @@ import sendNotifications from '../utilities/send_notifications';
 
 module.exports = (app, firebase) => {
   app.post('/message', (req, res) => {
-    const newMessage = req.body.message;
-    const groupId = req.body.groupId;
-    const messageSender = req.body.sender;
-    const priorityLevel = req.body.priorityLevel;
-
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
+        const newMessage = req.body.message;
+        const groupId = req.body.groupId;
+        const messageSender = req.body.sender;
+        const priorityLevel = req.body.priorityLevel;
+
         const db = firebase.database();
 
         // add new message to group messages and return new message key
