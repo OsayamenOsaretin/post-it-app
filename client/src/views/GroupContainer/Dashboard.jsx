393cb64b43f9a9d6a206c26ada86a83a74cf3805
yamen <osaretinosayamen@gmail.com>
2017-08-04 15:21:18 +0100

    update socket connection with heroku url

--- client/src/views/GroupContainer/Dashboard.jsx
+++ client/src/views/GroupContainer/Dashboard.jsx
@@ -9,7 +9,7 @@ import HeaderView from '../Header.jsx';
 // import PostItDispatcher from '../../data/PostItDispatcher';
 // import PostItActionTypes from '../../data/PostItActionTypes';
 
-const socket = io('http://localhost:6969');
+const socket = io('https://postit-app-develop.herokuapp.com/');
 
 // http://localhost:6969
 // https://postit-app-develop.herokuapp.com/
