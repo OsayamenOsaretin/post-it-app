3b572b73d37fe5e8e745bf2ea962107b7635f301
yamen <osaretinosayamen@outlook.com>
2017-08-10 09:46:26 +0100

    clear group routes and maps in firebase on event listener

--- server/routes/get_groups_route.js
+++ server/routes/get_groups_route.js
@@ -1,64 +1,54 @@
+
 // get a users groups
 
 module.exports = (app, firebase, io) => {
   app.get('/groups/:userId', (req, res) => {
-    // const users = new Map();
-    // const users = [];
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        // console.log(user);
-        // const userId = user.uid;
-        const currentUserId = req.params.userId;
-        // console.log(req);
-        console.log(userId);
-        // users.set(userId, user);
+        // user is signed in
+        const userId = user.uid;
+        const db = firebase.database();
 
-        // users.push(userId);
         // instantiate empty Map to hold groups
         const groups = new Map();
 
         let groupKeys = [];
-          // user is signed in
-          // const currentUserId = currentUser.uid;
-          const db = firebase.database();
 
-          // get user's groups
-          const groupsReference = db.ref(`/users/${currentUserId}/groups/`);
-          groupsReference.on('value', (snapshot) => {
-            groups.clear();
-            groupKeys = [];
+        // get user's groups
+        const groupsReference = db.ref(`/users/${userId}/groups/`);
+        groupsReference.orderByKey().on('value', (snapshot) => {
+          groups.clear();
+          groupKeys = [];
           // get the keys for each user's group
-            snapshot.forEach((groupSnapshot) => {
-              groupKeys.push(groupSnapshot.key);
-            });
-            // map to promises to asynchronously collect group info
-            const promises = groupKeys.map(groupKey => (
-              new Promise((resolve) => {
-                const groupReference = db.ref(`/groups/${groupKey}`);
-                groupReference.on('value', (snap) => {
-                  // add group info to list of groups
-                  groups.set(groupKey, snap.val());
-                  resolve();
-                });
-              })
-            ));
-            // collect resolved promises
-            Promise.all(promises)
-              .then(() => {
-                console.log(groups);
-                console.log(`newGroup${currentUserId}`);
-                io.emit(`newGroup${currentUserId}`, groups);
-              })
-              .catch((err) => {
-                io.emit('failedGroup', {
-                  error: err
-                });
-              });
+          snapshot.forEach((groupSnapshot) => {
+            groupKeys.push(groupSnapshot.key);
           });
-          return true;
-        // res.send({
-        //   message: 'Groups Returned'
-        // });
+
+          // map to promises to asynchronously collect group info
+          const promises = groupKeys.map(groupKey => (
+            new Promise((resolve) => {
+              const groupReference = db.ref(`/groups/${groupKey}`);
+              groupReference.on('value', (snap) => {
+                // add group info to list of groups
+                groups.set(groupKey, snap.val());
+                resolve();
+              });
+            })
+          ));
+          // collect resolved promises
+          Promise.all(promises)
+            .then(() => {
+              io.emit(`newGroup${userId}`, groups);
+            })
+            .catch((err) => {
+              io.emit('failedGroup', {
+                error: err
+              });
+            });
+        });
+        res.send({
+          message: 'Groups Returned'
+        });
       } else {
         res.status(403).send({
           // user is not signed in
@@ -68,4 +58,3 @@ module.exports = (app, firebase, io) => {
     });
   });
 };
-
