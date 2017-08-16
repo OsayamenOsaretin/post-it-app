fb0d95b32a48250175d188ffc80d5f3ee564b7a0
yamen <osaretinosayamen@outlook.com>
2017-08-16 10:13:42 +0100

    feedback-change from module.exports to export default

--- client/src/utility/bulkMessageRequest.js
+++ client/src/utility/bulkMessageRequest.js
@@ -2,15 +2,9 @@
 import getMessageAction from '../data/postItActions/getMessagesAction';
 
 export default (groups) => {
-  console.log('call bulk request message action');
   Object.values(groups).map(group => (
     getMessageAction({
       groupId: group[0]
     })
   ));
-  // Object.values(groups).map((group) => {
-  //   console.log(group[0]);
-  //   // console.log(value);
-  //   return true;
-  // });
 };
--- client/src/utility/getReadStatus.js
+++ client/src/utility/getReadStatus.js
@@ -5,9 +5,6 @@ export default (message, displayName) => {
     if (message.read[displayName]) {
       return true;
     }
-    // Object.keys(message.read).forEach((key) => {
-    //   console.log(key);
-    // });
   } else {
     return false;
   }
