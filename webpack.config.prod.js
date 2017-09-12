b0a5650d2ce4a49aeb83e3391b26d4487c6921bf
yamen <osaretinosayamen@outlook.com>
2017-09-12 12:37:02 +0100

    repair webpack build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -14,6 +14,11 @@ const config = {
   },
 
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
     new webpack.optimize.UglifyJsPlugin(),
     new HtmlWebpackPlugin({
       template: './client/src/index.html'
