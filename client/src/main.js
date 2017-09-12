d8a6585f4e1decfe02b3ed743c0d7642fde96e82
yamen <osaretinosayamen@outlook.com>
2017-09-12 11:44:26 +0100

    feedback(app.jsx): remove commented out code.

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import LandingPageContainer from 
-//  './views/LoginRegisterContainer/LandingPageContainer.jsx';
-// import RequestList from './views/GroupContainer/RequestList.jsx';
 import App from './views/App.jsx';
 import './index.css';
 
 /* global document */
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
