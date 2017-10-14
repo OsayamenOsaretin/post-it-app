5eda2425a1dbcff0caeb0e7dd06dbe290d2eaa91
yamen <osaretinosayamen@outlook.com>
2017-10-14 14:09:00 +0100

    refactor(utility): abstract get emails and numbers into seperate functions

--- client/src/utility/getUsersEmailsNumbers.js
+++ client/src/utility/getUsersEmailsNumbers.js
@@ -1,37 +1,40 @@
 // get emails for users in a group
 
+const getEmailNumbers = (userIds, db, emails, numbers) => {
+  return userIds.map(userId => (
+    new Promise((resolve) => {
+      // get user emails
+      const userEmailRef = db.ref(`users/${userId}/email`);
+      userEmailRef.once('value', (userEmail) => {
+        emails.push(userEmail.val());
+
+        // get user numbers
+        const userNumberRef = db.ref(`users/${userId}/number`);
+        userNumberRef.once('value', (userNumber) => {
+          numbers.push(userNumber.val());
+          resolve();
+        });
+      });
+    })
+  ));
+};
+
 export default (firebase, groupId, priorityLevel, callback) => {
   const db = firebase.database();
+  const userIds = [];
   const emails = [];
   const numbers = [];
-  const userIds = [];
 
   const groupReference = db.ref(`groups/${groupId}/users`);
-  groupReference.once('value', (snapshot) => {
+  return groupReference.once('value', (snapshot) => {
     // get the user Ids
     snapshot.forEach((userSnapshot) => {
       userIds.push(userSnapshot.key);
     });
-
-    const promises = userIds.map(userId => (
-      new Promise((resolve) => {
-        // get user emails
-        const userEmailRef = db.ref(`users/${userId}/email`);
-        userEmailRef.once('value', (userEmail) => {
-          emails.push(userEmail.val());
-
-          // get user numbers
-          const userNumberRef = db.ref(`users/${userId}/number`);
-          userNumberRef.once('value', (userNumber) => {
-            numbers.push(userNumber.val());
-            resolve();
-          });
-        });
-      })
-    ));
+    const promises = getEmailNumbers(userIds, db, emails, numbers);
     return Promise.all(promises)
-      .then(() => (
-        callback(emails, numbers, priorityLevel)
-      ));
+      .then(() => {
+        callback(emails, numbers, priorityLevel);
+      });
   });
 };
