2548fd4e9aba7a0010c48ecfbe3ac5eae41f50b3
yamen <osaretinosayamen@outlook.com>
2017-10-25 09:17:36 +0100

    refactor(get initials): use regex to extract user intials

--- client/src/utility/getSenderInitials.js
+++ client/src/utility/getSenderInitials.js
@@ -1,13 +1,5 @@
 // gets the initials of the message sender
 export default (sender) => {
-  let initials = '';
-  if (sender) {
-    const names = sender.split(' ');
-
-    names.forEach((name) => {
-      initials += name.charAt(0);
-    });
-  }
-
-  return initials;
+  const initials = sender.match(/\b\w/g) || [];
+  return ((initials.shift() || '') + (initials.pop() || '')).toUpperCase();
 };
