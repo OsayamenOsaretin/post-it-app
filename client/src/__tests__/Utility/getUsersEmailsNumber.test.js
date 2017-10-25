8164cbb4543d07786d618194e37aa2b2cb3c5e08
yamen <osaretinosayamen@outlook.com>
2017-10-25 14:15:56 +0100

    refactor(tests): fix broken tests

--- client/src/__tests__/Utility/getUsersEmailsNumber.test.js
+++ client/src/__tests__/Utility/getUsersEmailsNumber.test.js
@@ -15,15 +15,29 @@ describe('GetEmailsAndNumbers', () => {
   database.ref('users/testUserId/number').set('testPhonenumber');
 
   mockDatabase.flush();
-  it('should call the callback function', (done) => {
-    const mockCallback = jest.fn();
-    mockAuth.autoFlush();
-    mockDatabase.autoFlush();
+  it('should not call the callback function when priority is normal',
+    (done) => {
+      const mockCallback = jest.fn();
+      mockAuth.autoFlush();
+      mockDatabase.autoFlush();
 
-    getUsersEmailNumbers(firebase, 'testGroupId', 'normal', mockCallback)
-      .then(() => {
-        expect(mockCallback).toHaveBeenCalled();
-        done();
-      });
-  });
+      getUsersEmailNumbers(firebase, 'testGroupId', 'normal', mockCallback)
+        .then(() => {
+          expect(mockCallback).not.toHaveBeenCalled();
+          done();
+        });
+    });
+
+  it('should call the callback function when priority is not normal',
+    (done) => {
+      const mockCallback = jest.fn();
+      mockAuth.autoFlush();
+      mockDatabase.autoFlush();
+
+      getUsersEmailNumbers(firebase, 'testGroupId', 'critical', mockCallback)
+        .then(() => {
+          expect(mockCallback).toHaveBeenCalled();
+          done();
+        });
+    });
 });
--- client/src/utility/getSenderInitials.js
+++ client/src/utility/getSenderInitials.js
@@ -1,5 +1,8 @@
 // gets the initials of the message sender
 export default (sender) => {
-  const initials = sender.match(/\b\w/g) || [];
-  return ((initials.shift() || '') + (initials.pop() || '')).toUpperCase();
+  if (sender) {
+    const initials = sender.match(/\b\w/g) || [];
+    return ((initials.shift() || '') + (initials.pop() || ''));
+  }
+  return '';
 };
