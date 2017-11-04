b1edf8a3d65cf79c50fba8bfb67ae9392cc46ad9
yamen <osaretinosayamen@outlook.com>
2017-11-04 08:20:25 +0100

    style(make-responsive): make component responsive
    `` handle toggle state for mobile screen ``
    
    [Finished]

--- client/src/views/GroupContainer/GroupListView.jsx
+++ client/src/views/GroupContainer/GroupListView.jsx
@@ -1,33 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
+import FaClose from 'react-icons/lib/fa/caret-left';
+import FaBars from 'react-icons/lib/fa/bars';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import WelcomeView from './WelcomeView.jsx';
 import GroupList from './GroupList.jsx';
 import GroupItem from './GroupItem.jsx';
 
 /**
- * GroupList is a container for the list of groups, also doubles as a navlink
- * @param {Object} props
- * 
- * @return {void}
+ * Group list navigation and body
+ * @class GroupListview
+ * @extends {Component}
  */
-function GroupListView(props) {
-  return (
-    <BrowserRouter >
-      <div className="main-view">
-        <div className="group-list-nav">
-          <GroupList groups={props.groups} loading={props.loading}/>
+class GroupListView extends Component {
+  /**
+   * @param {Object} props
+   * 
+   * @returns {void}
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggleMode: 'toggle-out'
+    };
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  /**
+   * @return {void}
+   * @memberof GroupListView
+   */
+  handleToggleClick() {
+    this.setState({
+      toggleMode: this.state.toggleMode === 'toggle-in' ?
+        'toggle-out' : 'toggle-in'
+    });
+  }
+  /**
+ * @returns {void}
+ * @memberof GroupListView
+ */
+  render() {
+    return (
+      <BrowserRouter >
+        <div className="main-view">
+					<button
+            onClick={this.handleToggleClick}
+				    className={`toggle-button-${this.state.toggleMode} visible-sm visible-xs`}	
+					>
+						{this.state.toggleMode !== 'toggle-in' ? <FaBars /> : <FaClose />} 
+            </button>
+          <div className={`group-list-nav ${this.state.toggleMode}`}>
+            <GroupList groups={this.props.groups} loading={this.props.loading}/>
+          </div>
+          <div className="group-details">
+            <Switch>
+              <Route exact path='/groupBody/:groupId/:groupName'
+                component={groupProps => (
+									<GroupItem {...groupProps} />
+                )} />
+              <Route path='/' component={WelcomeView} />
+            </Switch>
+          </div>
         </div>
-        <div className="group-details">
-          <Switch>
-            <Route exact path='/groupBody/:groupId/:groupName'
-              component={groupProps => (
-                <GroupItem {...groupProps} />
-              )} />
-            <Route path='/' component={WelcomeView} />
-          </Switch>
-        </div>
-      </div>
-    </BrowserRouter>
-  );
+      </BrowserRouter>
+    );
+  }
 }
 export default GroupListView;
