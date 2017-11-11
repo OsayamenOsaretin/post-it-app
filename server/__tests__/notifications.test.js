861d2485343fdf553a740bedfaf7c978b787304c
yamen <osaretinosayamen@outlook.com>
2017-11-11 20:45:05 +0100

    tests(mock notifciationHelper): add mock for notification helper
    no need to call notification helper in tests cases
    
    [Finished]

--- server/__tests__/notifications.test.js
+++ server/__tests__/notifications.test.js
@@ -1,3 +1,6 @@
+/** 
+  *@jest-environment node
+  */
 import supertest from 'supertest';
 import app from '../server';
 
@@ -6,14 +9,17 @@ import app from '../server';
 
 const request = supertest(app);
 jest.unmock('superagent');
+jest.mock('../utilities/notificationHelper', () => jest.fn());
 
 
 describe('Notifications', () => {
   let emails, numbers;
+
   beforeEach(() => {
     emails = ['firstemail@email.com', 'secondemail@email.componentDidMount'];
     numbers = ['2348128186810'];
   });
+
   it('should send success message when notification route is hit', (done) => {
     request
       .post('/notifications')
