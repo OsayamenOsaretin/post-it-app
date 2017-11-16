b1336dd05c761d974868fdbc8e5adf11100be972
yamen <osaretinosayamen@outlook.com>
2017-11-16 08:45:11 +0100

    refactor(nav-component): use proper file extension
    [Finished]

---
+++ client/src/views/LoginRegisterContainer/Nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+/**
+ * @function Nav
+ * @memberof Nav.js
+ * 
+ * @returns {View} navlink component
+ * Nav holds routes to login and register components
+ */
+export default () => (<ul className = 'auth-nav'>
+  <li>
+    <NavLink exact activeClassName='active' to='/login'>
+      Log in
+    </NavLink>
+  </li>
+
+  <li>
+    <NavLink activeClassName='active' to='/register'>
+      Register
+    </NavLink>
+  </li>
+</ul>);
+
