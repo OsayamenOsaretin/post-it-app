eca7303be1eb7791d9ee47995394af0734f3a259
yamen <osaretinosayamen@outlook.com>
2017-11-22 14:37:37 +0100

    feedback(concrete-tests): use concrete payloads in test
    [Finished]

--- server/__tests__/notificationHelper.test.js
+++ server/__tests__/notificationHelper.test.js
@@ -8,7 +8,7 @@ describe('NotificationHelper', () => {
   let nexmoSpy;
   let transporterSpy;
   beforeEach(() => {
-    emails = ['firstemail@email.com', 'secondemail@email.componentDidMount'];
+    emails = ['firstemail@email.com', 'secondemail@email.com'];
     numbers = ['2348128186810'];
     transporterSpy = spyOn(transporter, 'sendMail');
     nexmoSpy = spyOn(message, 'sendSms');
@@ -23,12 +23,20 @@ describe('NotificationHelper', () => {
   it('should call email function only, for urgent messages', () => {
     notificationHelper(emails, numbers, 'urgent');
     expect(nexmoSpy).not.toHaveBeenCalled();
-    expect(transporterSpy).toHaveBeenCalled();
+    expect(transporterSpy).toHaveBeenCalledWith({
+      subject: 'New Message from Post-It',
+      text: 'You have an important message on Post-It, check it out',
+      from: 'postitbyyamen@gmail.com',
+      to: 'secondemail@email.com'
+    }
+    );
   });
 
   it('should call email and sms function for critical messages', () => {
     notificationHelper(emails, numbers, 'critical');
     expect(transporterSpy).toHaveBeenCalled();
-    expect(nexmoSpy).toHaveBeenCalled();
+    expect(nexmoSpy).toHaveBeenCalledWith('Post-It', '2348128186810'
+      , 'You have a critical message on Post-It, login to check now!',
+      { type: 'unicode' });
   });
 });
